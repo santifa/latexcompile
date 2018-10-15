fa0227e4864baa559fdb24155f1a9a5191c1ad33
Henrik Jürges <juerges.henrik@gmail.com>
2018-10-15 18:10:22 +0200

    Failure Api, Rewrite
    - included the failure crate
    - removed old code
    - restructured some parts/ new cmd struct

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,10 @@
 //!     // create the template map
 //!     let mut template = HashMap::new();
 //!     // provide the folder where the file for latex compiler are found
-//!     let template_folder = Path::new("template_folder").to_path_buf();
-//!     // create a new clean compiler enviroment
-//!     let compiler = LatexCompiler::new(template_folder, None, "main.tex").unwrap();
+//!     let template_folder = PathBuf::from("assets");
+//!     // create a new clean compiler enviroment and the compiler wrapper
+//!     let context = Context::new(template_folder, "card.tex")
+//!     let compiler = LatexCompiler::new(context).unwrap();
 //!     // run the underlying pdflatex or whatever
 //!     let result_path = compiler.run("example.pdf", &template);
 //!
@@ -33,11 +34,12 @@
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
+#[macro_use]
 extern crate log;
 extern crate regex;
 extern crate tempfile;
 
-use failure::{err_msg, Error};
+use failure::Error;
 use regex::Regex;
 use std::collections::HashMap;
 use std::ffi::OsStr;
@@ -48,7 +50,27 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 use tempfile::{tempdir, TempDir};
 
-#[derive(Debug)]
+/// Specify all error cases with the fail api.
+#[derive(Fail, Debug)]
+pub enum CompilerError {
+    #[fail(display = "Failed to apply template at stage {}.", _0)]
+    TemplatingError(String),
+    #[fail(display = "Failed to invoke latex compiler.")]
+    CompilationError,
+    #[fail(display = "Failed to change working enviroment.")]
+    EnviromentError,
+    #[fail(display = "Failed to create temporary context. {}", _0)]
+    ContextCreationError(#[cause] std::io::Error),
+    #[fail(display = "{}", _0)]
+    Io(#[cause] std::io::Error),
+}
+
+/// result type alias idiom
+type Result<T> = std::result::Result<T, CompilerError>;
+
+/// Hold the command line program which is used and the required arguments.
+/// As default `pdflatex -interaction=nonstopmode` is used.
+#[derive(Debug, PartialEq)]
 struct Cmd {
     cmd: String,
     args: Vec<String>,
@@ -57,18 +79,17 @@ struct Cmd {
 impl Default for Cmd {
     fn default() -> Self {
         Cmd {
-            cmd: "".into(),
-            args: vec!["".into()],
+            cmd: "pdflatex".into(),
+            args: vec!["-interaction=nonstopmode".into()],
         }
     }
 }
-// .arg("-interaction=nonstopmode")
-//            .arg(&self.ctx.main_file)
 
-/// Provide the context informations for the templating
-/// and compile process.
+/// The context provides the clean envoriment for a compile process.
+/// It should be created new for every run since it destroys the
+/// temporary working directory.
 #[derive(Debug)]
-struct Context {
+pub struct Context {
     working_dir: TempDir,
     source_dir: PathBuf,
     main_file: String, // The main file for latex
@@ -76,86 +97,99 @@ struct Context {
 }
 
 impl Context {
-    /// Create a new LatexCompiler which uses the
-    fn new(source: PathBuf, cli: Option<String>, file: &str) -> Result<Context> {
-        let dir = tempdir().chain_err(|| "Unable to create temporary enviroment.")?;
+    /// Create a new basic context or throw an error if we have no access to the temp directory.
+    /// Provide only the file name as main_file which can be found under the template source directory.
+    pub fn new(source: PathBuf, main_file: &str) -> Result<Context> {
+        let dir = tempdir().map_err(CompilerError::Io)?;
         Ok(Context {
             working_dir: dir,
-            cmd: cli.unwrap_or("pdflatex".into()),
+            cmd: Cmd::default(),
             source_dir: source,
-            main_file: file.into(),
+            main_file: main_file.into(),
         })
+    }
+
+    /// Overwrite the default cmd `pdflatex`
+    pub fn with_cmd(mut self, cmd: &str) -> Self {
+        self.cmd.cmd = cmd.into();
+        self
+    }
+
+    /// Clean the arguments list and add a new argument.
+    /// Use add_arg to add further arguments
+    pub fn with_args(mut self, cmd: &str) -> Self {
+        self.cmd.args = vec![cmd.into()];
+        self
+    }
+
+    /// Add a new argument to the cmd.
+    pub fn add_arg(mut self, cmd: &str) -> Self {
+        self.cmd.args.push(cmd.into());
+        self
+    }
+
+    /// build the command line
+    fn get_cmd(&self) -> Command {
+        let mut cmd = Command::new(&self.cmd.cmd);
+        cmd.args(&self.cmd.args);
+        cmd.arg(&self.main_file);
+        cmd
+    }
+
+    /// get the name of the result file
+    fn get_result_name(&self, suffix: &str) -> Option<PathBuf> {
+        Path::new(&self.main_file).file_stem().and_then(OsStr::to_str)
+            .and_then(|name| Some(self.working_dir.path().join(name.to_string() + suffix)))
     }
 }
 
-/// This struct defines a clean enviroment in which
-/// the latex files are compiled. It takes a command line, how call latex
-/// and provides helper for getting the result back and cleaning up the enviroment.
+/// The latex compiler wrapper which includes
+/// a template processor.
 pub struct LatexCompiler {
     ctx: Context,
     tp: TemplateProcessor,
 }
 
 impl LatexCompiler {
-    pub fn new(source: PathBuf, cli: Option<String>, file: &str) -> Result<LatexCompiler> {
+    pub fn new(ctx: Context) -> Result<LatexCompiler> {
         Ok(LatexCompiler {
-            ctx: Context::new(source, cli, file)?,
+            ctx: ctx,
             tp: TemplateProcessor::new()?,
         })
     }
+
+    /// Switch the working directory and return
+    /// the old value
+    fn switch_env(path: &Path) -> Result<PathBuf> {
+        let old = ::std::env::current_dir().or(Err(CompilerError::EnviromentError))?;
+        ::std::env::set_current_dir(path).or(Err(CompilerError::EnviromentError))?;
+        Ok(old.as_path().to_path_buf())
+    }
+
 
     pub fn run(&self, output_name: &str, dict: &HashMap<String, String>) -> Result<()> {
         // prepare sources
         self.tp.process_sources(&self.ctx, &dict);
 
         // change into new working directory
-        let current_dir = ::std::env::current_dir().chain_err(|| "Unable to get current dir")?;
-        ::std::env::set_current_dir(self.ctx.working_dir.path())
-            .chain_err(|| "Unable to switch to enviroment.")?;
+        let cur_dir = LatexCompiler::switch_env(self.ctx.working_dir.path())?;
 
-        // run latex on main pdf
-        Command::new(&self.ctx.cmd)
-            .arg("-interaction=nonstopmode")
-            .arg(&self.ctx.main_file)
-            .status()
-            .chain_err(|| "Failed to run latex.")?;
-
-        // do a rerun
-        Command::new(&self.ctx.cmd)
-            .arg("-interaction=nonstopmode")
-            .arg(&self.ctx.main_file)
-            .status()
-            .chain_err(|| "Failed to run latex.")?;
+        // first and second run
+        self.ctx.get_cmd().status().map_err(CompilerError::Io)?;
+        self.ctx.get_cmd().status().map_err(CompilerError::Io)?;
 
         // get name of the result file
-        let base = Path::new(&self.ctx.main_file)
-            .file_stem()
-            .and_then(OsStr::to_str)
-            .chain_err(|| "Unable to get base name.")?;
-        let result = self.ctx.working_dir.path().join(base.to_string() + ".pdf");
+        let result_name = self.ctx.get_result_name(".pdf").ok_or(CompilerError::CompilationError)?;
 
-        // return result
-        let output = current_dir.join(output_name);
-        copy(result.clone(), output.clone()).chain_err(|| {
-            format!(
-                "Unable to copy result {:?} to location {:?}",
-                result, output
-            )
-        })?;
+        // copy result file
+        let output = cur_dir.join(output_name);
+        copy(result_name, output)
+            .map_err(CompilerError::Io)?;
 
         // reset env
-        ::std::env::set_current_dir(current_dir).chain_err(|| "Unable to switch Enviroment back.")
-    }
-
-    /*  /// This is a handy method to add partial documents to the enviroment.
-    /// These are excluded from template processing.
-    pub fn write_latex_document(&self, doc: Document, name: &str) -> Result<()> {
-        let file = self.ctx.working_dir.path().join(name);
-        let rendered = print(&doc).chain_err(|| "Unable to render file.")?;
-        let mut f = File::create(file).chain_err(|| "Unable to open file.")?;
-        write!(f, "{}", rendered).chain_err(|| "Unable to write file.")?;
+        LatexCompiler::switch_env(&cur_dir)?;
         Ok(())
-    }*/
+    }
 }
 
 /// The processor takes latex files as input and replaces
@@ -169,21 +203,19 @@ impl TemplateProcessor {
     /// Characters allowed as variable names: "a-zAZ0-9-_"
     fn new() -> Result<TemplateProcessor> {
         Ok(TemplateProcessor {
-            regex: Regex::new(r"##[a-z|A-Z|\d|-|_]+##")
-                .chain_err(|| "Unable to compile variabel regex")?,
+            regex: Regex::new(r"##[a-z|A-Z|\d|-|_]+##").or(Err(CompilerError::TemplatingError("Failed to compile regex.".to_string())))?,
         })
     }
 
     /// Replace variables for all files within the template path and
     /// copy the results into the created enviroment.
     fn process_sources(&self, ctx: &Context, dict: &HashMap<String, String>) -> Result<()> {
-        let paths = read_dir(&ctx.source_dir).chain_err(|| "Failed to read template directory.")?;
+        let paths = read_dir(&ctx.source_dir).or(Err(CompilerError::TemplatingError("Failed to read template directory.".to_string())))?;
         for path in paths {
-            let src_file = path.chain_err(|| "Unable to get source file path.")?.path();
+            let src_file = path.or(Err(CompilerError::TemplatingError("Unable to get source file path.".to_string())))?.path();
             let dst_file = ctx.working_dir.path().join(
                 src_file
-                    .strip_prefix(&ctx.source_dir)
-                    .chain_err(|| "Unable to strip common prefix.")?,
+                    .strip_prefix(&ctx.source_dir).or(Err(CompilerError::TemplatingError("Unable to strip prefix.".to_string())))?
             );
 
             self.process_file(&src_file, &dst_file, &dict)?;
@@ -197,18 +229,18 @@ impl TemplateProcessor {
     /// actual value.
     fn process_file(&self, src: &Path, dst: &Path, dict: &HashMap<String, String>) -> Result<()> {
         let mut content = String::new();
-        let mut src_file = File::open(src).chain_err(|| "Unable to open source file.")?;
+        let mut src_file = File::open(src).or(Err(CompilerError::TemplatingError("Unable to open source file.".to_string())))?;
 
         match src_file.read_to_string(&mut content) {
             Err(_) => {
                 // maybe binary data, so just copy it
-                copy(&src, &dst).chain_err(|| "Unable to copy file.")?;
+                copy(&src, &dst).or(Err(CompilerError::TemplatingError("Unable to copy file.".to_string())))?;
             }
             Ok(_) => {
                 let replaced_content = self.process_placeholders(&content, &dict)?;
                 File::create(dst)
                     .and_then(|mut f| f.write_all(replaced_content.as_bytes()))
-                    .chain_err(|| "Unable to create destination file.")?;
+                    .or(Err(CompilerError::TemplatingError("Unable to create destination file.".to_string())))?;
             }
         }
 
@@ -226,7 +258,7 @@ impl TemplateProcessor {
 
         let mut running_index = 0;
         for c in self.regex.captures_iter(&content) {
-            let _match = c.get(0).chain_err(|| "Unable to get regex match.")?;
+            let _match = c.get(0).unwrap(); //ok_or(Err(CompilerError::TemplatingError("Unable to get regex match.".to_string())))?;
             let key = &content[_match.start() + 2.._match.end() - 2];
             replaced += &content[running_index.._match.start()];
             println!("found {:?}\n", key);
@@ -247,8 +279,29 @@ impl TemplateProcessor {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_context_cmd() {
+        let mut context = Context::new(PathBuf::new(), "".into());
+        assert!(context.is_ok());
+        let context = context.unwrap().with_cmd("latexmk").with_args("arg1").add_arg("arg2");
+        let ctx = Cmd {
+            cmd: "latexmk".into(),
+            args: vec!["arg1".into(), "arg2".into()],
+        };
+        assert_eq!(context.cmd, ctx);
+    }
+
+    #[test]
+    fn test_templating() {
+        let assets = PathBuf::from("assets");
+        let context = Context::new(assets, "card.tex".into());
+        assert!(context.is_ok());
+        let templating = TemplateProcessor::new();
+        assert!(templating.is_ok());
+        let map = HashMap::new();
+        let res = templating.unwrap().process_sources(&context.unwrap(), &map);
+        assert!(res.is_ok());
     }
 }
